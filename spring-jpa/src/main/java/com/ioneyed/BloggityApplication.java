58328abce5a76429d71c7debd2f6171a8d709c01
Robert Buchanan <robert.buchanan@kroger.com>
2016-12-27 23:55:45 -0500

    Initial Commit - Base Spring Application from start.spring.io with web, security, h2, devtools, thymeleaf

---
+++ spring-jpa/src/main/java/com/ioneyed/BloggityApplication.java
@@ -0,0 +1,12 @@
+package com.ioneyed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggityApplication.class, args);
+	}
+}
